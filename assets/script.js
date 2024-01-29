f15635383a846c8cf07ba985d1c6a7afa271c4b4
Selene <149156667+joanapq@users.noreply.github.com>
2024-01-29 13:30:02 +0000

    made start on function to search by city and display current weather data

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,19 @@
 let searchForm = $('#search-form');
 
+searchForm.submit(submitSearch);
+
 function submitSearch (event) {
     event.preventDefault();
-    var search = $('#search-input').val().trim();
-    console.log(search);
+    let city = $('#search-input').val().trim();
+    console.log(city);
+
+    let APIKey = "ec65626f17967dd632fbed28f71be01e";
+    
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid" + APIKey;
 }
 
-$('#search-form').on('click', submitSearch);
+
+
+
+
+
